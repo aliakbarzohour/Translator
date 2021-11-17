ad6a24ccbec7ecd8bd14c8b6aa737a684cfa14fc
aliakbarzohour <aliakbar.zohour@gmail.com>
2021-11-17 19:50:03 +0330

    adding colorama in project

--- translator.py
+++ translator.py
@@ -1,8 +1,12 @@
 from translate import Translator
+from colorama import Fore,init
 
-user = input(" [ ~ ] Enter text for translating : ")
+print("Welcome to Translator\n")
+user = input(Fore.YELLOW+" [ * ] "+Fore.WHITE+"Enter text for translating : ")
 
 options = Translator(from_lang='en', to_lang='persian')
 translate = options.translate(user)
+
+print(Fore.YELLOW+'----------'+Fore.WHITE+' Translate '+Fore.YELLOW+'----------')
 
 print(translate)
